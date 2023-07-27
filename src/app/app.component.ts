b5f1b7948e18579063f8caccd2b8fa4cfaba1cee
NastuaBudko <nastuabudko@gmail.com>
2023-07-27 22:25:19 +0300

    created angular password strength checker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  password: string = '';
+  passwordStrength: string = '';
+
+  calculatePasswordStrength() {
+    // Implement your password strength calculation logic here
+    // For example, you can define different conditions to determine the strength
+    if (!this.password) {
+      this.passwordStrength = ''; // Empty password, no strength
+    } else if (this.password.length < 6) {
+      this.passwordStrength = 'easy'; // Password with less than 6 characters
+    } else if (this.password.length < 10) {
+      this.passwordStrength = 'medium'; // Password with 6 to 9 characters
+    } else {
+      this.passwordStrength = 'strong'; // Password with 10 or more characters
+    }
+  }
 }
